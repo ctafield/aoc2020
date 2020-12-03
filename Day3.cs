2cc78b81a99330aa783ec6093575b87c795acee3
Christopher Field <ctafield@gmail.com>
2020-12-03 08:14:11 +0000

    Day 3 part 2

--- Day3.cs
+++ Day3.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace aoc2020
 {
@@ -15,25 +13,48 @@ namespace aoc2020
 
             var values = LoadInput<string>(@"input\day3.txt");
             Part1(values.ToArray());
+            Part2(values.ToArray());
         }
 
         private void Part1(string[] values)
         {
+            var trees = ProcessTraversal(values, 3, 1);
+            Console.WriteLine($"Part 1 - Trees = {trees}");
+        }
+
+        private void Part2(string[] values)
+        {
+            var val1 = ProcessTraversal(values, 1, 1);
+            var val2 = ProcessTraversal(values, 3, 1);
+            var val3 = ProcessTraversal(values, 5, 1);
+            var val4 = ProcessTraversal(values, 7, 1);
+            var val5 = ProcessTraversal(values, 1, 2);
+
+            var trees = val1 * val2 * val3 * val4 * val5;
+
+            Console.WriteLine($"Part 2 - Trees = {trees}");
+        }
+
+        private int ProcessTraversal(string[] values, int rightStep, int downStep)
+        {
             var col = 0;
             var trees = 0;
-            
-            for (var row = 0; row < values.Count(); row++) {
+
+            for (var row = 0; row < values.Count(); row += downStep)
+            {
                 var position = values[row][col];
-                if (position == Tree) {
+                if (position == Tree)
+                {
                     trees++;
                 }
-                col +=3;
-                if (col >= values[row].Length) {
+                col += rightStep;
+                if (col >= values[row].Length)
+                {
                     col -= values[row].Length;
                 }
             }
 
-            Console.WriteLine($"Trees = {trees}");
+            return trees;
         }
     }
 }
